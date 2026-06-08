1472c011be69ccaf951647191fda7a5bb2a54788
tommiedevelops <thomas.dizon25@gmail.com>
2026-06-08 11:33:27 +1000

    refactor: cleaned up app.h

--- core/Application/App.h
+++ core/Application/App.h
@@ -6,6 +6,9 @@
 #include "GameTime.h"
 #include "Layer.h"
 
+#include <vector>
+#include <memory>
+
 namespace PhysicsEngine
 {
 	using Layers = std::vector<std::shared_ptr<Layer>>;
@@ -16,47 +19,37 @@ namespace PhysicsEngine
 		static void Init(WindowProperties& windowProperties);
 		static App* GetInstance();
 
-		void Run();
-		void OnEvent(Event& e);
+		App(const App&)             = delete;
+		App& operator=(const App&)  = delete;
 
-		Window* GetWindow()
-		{
-			return &m_Window;
-		}
+		void   Run();
+		void   MainLoop();
+		void   OnEvent(Event& e);
+		void   PushLayer(std::shared_ptr<Layer> layer);
 
-		// no copies and moves
-		App(const App&) = delete;
-		App& operator=(const App&) = delete;
-
-		AssetManager* GetAssetsRef()
-		{
-			return &m_Assets;
-		}
-
-		void PushLayer(std::shared_ptr<Layer> layer);
+		Window*       GetWindow()    { return &m_Window; }
+		AssetManager* GetAssetsRef() { return &m_Assets; }
 
 		template <typename T>
 		T* GetLayer()
 		{
 			static_assert(std::derived_from<T, Layer>, "T must derive from Layer");
-
 			for (const auto& layer : m_Layers)
 			{
 				if (T* result = dynamic_cast<T*>(layer.get()))
 					return result;
 			}
-
 			return nullptr;
 		}
 		
-		void MainLoop();
 	private:
-
 		App(WindowProperties& windowProperties);
+
 		Layers  			 m_Layers;
 		Window               m_Window;
 		WindowProperties&    m_WindowProperties;
 		AssetManager         m_Assets;
 		GameTime             m_GameTime;
+
 	};
 }
